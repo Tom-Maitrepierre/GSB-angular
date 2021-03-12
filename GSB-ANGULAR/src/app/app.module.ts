e7703d25b4a70c51571f00a769cdab0666e13a18
maxime-ui <toupiemax01@hotmail.com>
2021-03-12 11:49:49 +0100

    ajout routing

--- GSB-ANGULAR/src/app/app.module.ts
+++ GSB-ANGULAR/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MedecinsComponent } from './medecins/medecins.component';
@@ -9,7 +10,17 @@ import { RapportsComponent } from './rapports/rapports.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { ModifierRapportsComponent } from './rapports/modifier-rapports/modifier-rapports.component';
 import { CreerRapportsComponent } from './rapports/creer-rapports/creer-rapports.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+
+const appRoutes : Routes =[
+  {path: 'rapports',component:RapportsComponent },
+  {path: 'rapports-modifier',component:ModifierRapportsComponent },
+  {path: 'rapports-modifier',component:CreerRapportsComponent },
+]
+
+  
 
 @NgModule({
   declarations: [
@@ -23,7 +34,9 @@ import { CreerRapportsComponent } from './rapports/creer-rapports/creer-rapports
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
