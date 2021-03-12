e59d8779f684f829971d9ad8b7b8eb59f99e100e
SAGLAMER <trk.0506@hotmail.com>
2021-03-12 10:41:24 +0100

    menu

--- GSB-ANGULAR/src/app/app-routing.module.ts
+++ GSB-ANGULAR/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {MedecinsComponent} from './medecins/medecins.component';
+import {MedicamentsComponent} from './medicaments/medicaments.component';
+import {RapportsComponent} from './rapports/rapports.component';
+
+const routes: Routes = [
+    {path:'medecins',component:MedecinsComponent},
+    {path:'medicaments',component:MedicamentsComponent},
+    {path:'rapports',component:RapportsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GSB-ANGULAR/src/app/app.module.ts
+++ GSB-ANGULAR/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MedecinsComponent } from './medecins/medecins.component';
+import { MedicamentsComponent } from './medicaments/medicaments.component';
+import { RapportsComponent } from './rapports/rapports.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedecinsComponent,
+    MedicamentsComponent,
+    RapportsComponent
   ],
   imports: [
     BrowserModule,
